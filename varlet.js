ab21b6eb21034d8703112a7e8f8ab6e669dcc1a7
AyushmanBrijwal <76858741+AyushmanBrijwal@users.noreply.github.com>
2024-02-13 21:38:22 +0530

    varlet.js

---
+++ varlet.js
@@ -0,0 +1,11 @@
+var myDog = "Montie";
+console.log(myDog);
+
+/* let myDog = "Montie";
+console.log(myDog); same as var */
+
+/* const myDog = "Montie";
+console.log(myDog); will throw error */
+
+myDog = "Blacky";
+console.log(myDog);
