7fb4d0efd724812c66ceb0afda66ed742b5e483e
AyushmanBrijwal <76858741+AyushmanBrijwal@users.noreply.github.com>
2024-02-14 19:05:51 +0530

    classes.js

---
+++ classes.js
@@ -0,0 +1,27 @@
+class Model {
+  constructor(){
+    this.modelno = 'R8';
+  }
+  printModel(){
+    console.log(this.modelno);
+  }
+}
+
+/*const dog = new Dog();
+dog.printGender(); */
+
+
+class Car extends Model {
+  constructor(){
+    super(); /*added to create a super constructor */
+    this.brand = 'Audi';
+    this.modelno = 'A9'; /* its given priority */
+  }
+  printBrand(){
+    console.log(this.brand);
+  }
+}
+
+const car = new Car();
+car.printBrand();
+car.printModel();
